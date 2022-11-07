72d872d33f8ef960047f51bfeae94e7d16fafae7
SEBASTIEN_ARTESI <SEBASTIEN.ARTESI@gmail.com>
2022-11-07 20:49:36 +0100

    Express 05 Delete

--- app.js
+++ app.js
@@ -17,10 +17,14 @@ const userHandlers = require("./userHandlers");
 app.post("/api/movies", movieHandlers.postMovie);
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+
 app.post("/api/users", userHandlers.postUser);
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
-
+app.put("/api/users/:id", userHandlers.updateUser);
+app.delete("/api/users/:id", userHandlers.deleteUser);
 
 app.listen(port, (err) => {
   if (err) {
--- userHandlers.js
+++ userHandlers.js
@@ -48,8 +48,72 @@ const getUsers = (req, res) => {
       });
   };
   
+  const updateUser = (req, res) => {
+    const id = parseInt(req.params.id);
+    const {firstname, lastname, email, city, language} = req.body;
+  
+    database
+      .query(
+        "update users set firstname = ?, lastname = ?, email = ?, city = ?, language = ? where id = ?",
+        [firstname, lastname, email, city, language, id]
+      )
+      .then(([result]) => {
+        if (result.affectedRows === 0) {
+          res.status(404).send("Not Found");
+        } else {
+          res.sendStatus(204);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error editing the user");
+      });
+  };
+
+  const updateMovie = (req, res) => {
+    const id = parseInt(req.params.id);
+    const {title, director, year, color, duration} = req.body;
+    
+    database
+    .query(
+      "update movies set title = ?, director = ?, year = ?, color = ?, duration = ? where id = ?",
+      [title, director, year, color, duration, id]
+      )
+      .then(([result]) => {
+        if (result.affectedRows === 0) {
+          res.status(404).send("Not Found");
+        } else {
+          res.sendStatus(204);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error editing the movie");
+      });
+    };
+    
+    const deleteUser = (req, res) => {
+      const id = parseInt(req.params.id);
+    
+      database
+        .query("delete from users where id = ?", [id])
+        .then(([result]) => {
+          if (result.affectedRows === 0) {
+            res.status(404).send("Not Found");
+          } else {
+            res.sendStatus(204);
+          }
+        })
+        .catch((err) => {
+          console.error(err);
+          res.status(500).send("Error deleting the user");
+        });
+    };
+
   module.exports = {
     getUsers,
     getUserById,
     postUser,
+    updateUser,
+    deleteUser,
   };
